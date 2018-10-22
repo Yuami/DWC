cffa0b556f4a7a31790da0d5c22df4900e2d5ba0
Yuami <j_fornes_1999@hotmail.com>
2018-10-22 20:47:43 +0200

    ACT 18 JQUERY NO FUNCIONA

--- JQUERY/ACT 18/JS/script.js
+++ JQUERY/ACT 18/JS/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
+    let list = $("#list-fields");
+    let add = $("#addItem");
 
+    add.click(function () {
+        list.append(
+            "<li>" +
+            "<input type='text'>" +
+            "<button>REMOVE</button>" +
+            "</li>"
+        );
+    });
+
+    $("li").on("click", ".btn-rmv",remove);
 });
+
+function remove() {
+    $(this).parent().remove();
+}
