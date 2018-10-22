e137be44254c07bc0b97c49b0e4dd8d200be164a
Yuami <j_fornes_1999@hotmail.com>
2018-10-22 20:15:33 +0200

    ACT 17 DONE

--- JQUERY/ACT 17/JS/script.js
+++ JQUERY/ACT 17/JS/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-
+    $("input:text").blur(function () {
+        let s = $(this).val();
+        if (s === ""){
+            alert("It can't be empty");
+        }
+    })
 });
