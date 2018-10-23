6d5a5196eb3f8e7e14103056e9312b4c369b94e9
Yuami <j_fornes_1999@hotmail.com>
2018-10-23 21:56:01 +0200

    ACT 23 DONE

--- JQUERY/ACT 23/JS/script.js
+++ JQUERY/ACT 23/JS/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-
+    $(".link").each(bold);
 });
+
+function bold() {
+    let s = $(this).attr("href");
+    if (s.indexOf(".es") !== -1){
+        $(this).css("font-weight", "bold");
+    }
+}
